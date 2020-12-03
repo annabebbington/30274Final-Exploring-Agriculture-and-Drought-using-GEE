2aa24135d196470656453701961083b360e9bf40
annabebbington <45781344+annabebbington@users.noreply.github.com>
2020-12-02 20:01:12 -0500

    Update FinalScript.py

--- FinalScript.py
+++ FinalScript.py
@@ -1,44 +1,31 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2513
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
-{\colortbl;\red255\green255\blue255;\red27\green31\blue34;}
-{\*\expandedcolortbl;;\cssrgb\c14118\c16078\c18039;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl380\partightenfactor0
+// Import Cropland Data 
+var crop = ee.Image('USGS/GFSAD1000_V1');
 
-\f0\fs27\fsmilli13600 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 // Import Cropland Data \
-var crop = ee.Image('USGS/GFSAD1000_V1');\
-\
-// Create visualization parameters for crop layer. Color palette suggested in metadata. \
-var cropMaskVis = \{\
-  min:0, \
-  max:5, \
-  palette: ['black', 'orange', 'brown', '02a50f', 'green', 'yellow\'92];\
-// Add crop layer to map\
-Map.addLayer(crop, cropMaskVis, 'Cropland\'92);\
-\
-\pard\pardeftab720\sl380\partightenfactor0
-\cf2 // Center the map on the geometry variable, and set the zoom level\
-Map.centerObject(SiouxFalls, 5); \
-\pard\pardeftab720\sl380\partightenfactor0
-\cf2 \
-\pard\pardeftab720\sl380\partightenfactor0
-\cf2 // Color Palette for rainfed cropland \
-var cropMaskVis2 = \{\
-  min: 2.0,\
-  max: 5.0,\
-  palette: ['White', 'DarkGreen', 'SeaGreen', 'DarkSeaGreen'],\
-\};\
-// Add Rainfed cropland map \
-Map.addLayer(crop, cropMaskVis2, 'Rainfed Croplands, dif');\
-\pard\pardeftab720\sl380\partightenfactor0
-\cf2 \
-\pard\pardeftab720\sl380\partightenfactor0
-\cf2 // Create a variable of only rainfed agriculture\
-var rainfed = crop.gte(3);\
-print(rainfed);\
-\
+// Create visualization parameters for crop layer. Color palette suggested in metadata. 
+var cropMaskVis = {
+  min:0, 
+  max:5, 
+  palette: ['black', 'orange', 'brown', '02a50f', 'green', 'yellow'],
+};
+// Add crop layer to map
+Map.addLayer(crop, cropMaskVis, 'Cropland\'92);
+             
+// Center the map on the geometry variable, and set the zoom level
+Map.centerObject(SiouxFalls, 5); 
+          
+// Color Palette for rainfed cropland 
+var cropMaskVis2 = {
+  min: 2.0,
+  max: 5.0,
+  palette: ['White', 'DarkGreen', 'SeaGreen', 'DarkSeaGreen'],
+};
+// Add Rainfed cropland map 
+Map.addLayer(crop, cropMaskVis2, 'Rainfed Croplands, dif');
+ 
+// Create a variable of only rainfed agriculture
+var rainfed = crop.gte(3);
+print(rainfed);
+
 Map.addLayer(rainfed, \{palette: ['White', 'OliveDrab']\}, 'Rainfed Boolean\'92);\
 \
 // Import Drought Data \
